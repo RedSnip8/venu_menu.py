3a2cf45770f7793532b50c5b55889dfe5657d303
Redsnip8 <FCipolone@gmail.com>
2019-08-21 00:56:20 -0400

    Path change
    admin path change for security.

--- venumenu/urls.py
+++ venumenu/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(r'venu_admin/', admin.site.urls, name='admin'),
     path('', include('pages.urls')),
 ]
